f8073b6ee943e0d35d87683d2898c6f1f4a9b07f
AryanM65 <aryanmahajan600@gmail.com>
2025-05-17 21:35:59 +0530

    added cloudinary setup

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
+const fileupload = require('express-fileupload');
+const files = require('./routes/fileUpload');
+app.use(fileupload({
+    useTempFiles : true,
+    tempFileDir : '/tmp/'
+}));
 const user = require('./routes/user');
 const PORT = process.env.PORT || 4000;
 
 require('./config/database').connect();
+require('./config/cloudinary').cloudinaryConnect();
 
 app.use('/api/v1', user);
+app.use('/api/v1/upload', files);
 
 app.listen(PORT, () => {
     console.log(`Server live at ${PORT}`);
